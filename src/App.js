906d4118626ab5a70f88b1f864c71cf97662632a
Cristina Lester <cristina.deelester@gmail.com>
2022-02-15 16:57:55 -0800

    added props to children

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import "./App.css";
 import { SplitScreen } from "./split-screen/components/SplitScreen";
 import styled from "styled-components";
 
-const LeftHandComponent = () => {
-  return <h1 style={{ background: "green" }}>Left</h1>;
+const LeftHandComponent = ({ name }) => {
+  return <h1 style={{ background: "green" }}>{name}</h1>;
 };
 
-const RightHandComponent = () => {
-  return <h1 style={{ background: "red" }}>Right</h1>;
+const RightHandComponent = ({ message }) => {
+  return <h1 style={{ background: "red" }}>{message}</h1>;
 };
 
 const AppWrapper = styled.div`
@@ -22,8 +22,8 @@ function App() {
   return (
     <AppWrapper>
       <SplitScreen children leftWeight={1} rightWeight={3}>
-        <LeftHandComponent />
-        <RightHandComponent />
+        <LeftHandComponent name="Cree" />
+        <RightHandComponent message="E' babba" />
       </SplitScreen>
     </AppWrapper>
   );
