ea5339c061e14de013a7a5f4bcbca59e1064a852
Cristina Lester <cristina.deelester@gmail.com>
2022-02-17 16:51:59 -0800

    added controlled form validation

--- src/components/controlled-uncontrolled-components/ControlledForm.js
+++ src/components/controlled-uncontrolled-components/ControlledForm.js
@@ -1,24 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function ControlledForm(props) {
   const [name, setName] = useState("");
   const [age, setAge] = useState();
   const [hairColor, setHairColor] = useState("");
+  const [nameInputErr, setNameInputErr] = useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(name);
-  };
+  useEffect(() => {
+    if (name.length < 2)
+      setNameInputErr("Name must be at least 2 characters long");
+    else setNameInputErr("");
+  }, [name]);
 
   /**
    * In controlled forms the values of the inputs are controlled by the state,
    * while in uncontrolled forms each input has its own state which we can access after
    * a submit event occurs.
    * The controlled component is creating a two way binding bewteen the value of the input fields
-   * and the value of the states
+   * and the value of the states.
+   * Controlled forms are good for live form validation
    */
   return (
-    <form onSubmit={handleSubmit}>
+    <form>
       <input
         type="text"
         name="name"
@@ -27,6 +30,7 @@ function ControlledForm(props) {
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
+      <small>{nameInputErr}</small>
       <input
         type="number"
         name="age"
@@ -43,7 +47,10 @@ function ControlledForm(props) {
         placeholder="hair color"
         onChange={(e) => setHairColor(e.target.value)}
       />
-      <input type="submit" value="Submit" />
+      {/**when we are not relying on the onSubmit event we can just use a button
+       * instead of an input with type "submit"
+       */}
+      <button>Submit</button>
     </form>
   );
 }
